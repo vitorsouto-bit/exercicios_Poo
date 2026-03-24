e75931d28c18052c9f8cc94f08f8b0fd7a6b120f
vitor hugo <vitorhugosilvasouto@gmail.com>
2026-03-23 21:02:52 -0400

    primeiro commit

---
+++ src/Carro.java
@@ -0,0 +1,14 @@
+public class Carro {
+    String marca = "honda";
+    String modelo = "civic";
+    String ano = "2023";
+
+
+  public void Carro1() {
+        String marca = "toyota";
+        String modelo = "corola";
+        String ano = "2025";
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+Carro carro = new Carro();
+Carro carro1 = new Carro();
+
+
+
+
+        System.out.print( "meu carro é do ano de " + carro.ano + " do modelo  " +  carro.modelo +" da "+ carro.marca + "\n");
+
+        System.out.println( "meu carro é do ano de " + carro1.ano + " do modelo  " + carro1.modelo +" da "+ carro1.marca);
+
+
+    }
+}
